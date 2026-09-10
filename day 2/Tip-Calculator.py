b3cdd52c4b463f0cef84c8446fc8a9e441c2dd3c
Amin El Filali <123209161+elfilaliamin@users.noreply.github.com>
2026-09-10 23:14:03 +0100

    Fix tip calculation and format output
    Calculate the total bill including tip and split among people.

--- day 2/Tip-Calculator.py
+++ day 2/Tip-Calculator.py
@@ -3,5 +3,7 @@ bill = input("What was the total bill? ")
 tip = input("How much tip would you like to give? 10, 12, or 15? ")
 people = input("How many people to split the bill? ")
 
-result = float(bill)/float(people) + float(tip)
-print(f"Each Person should pay: {result}")
+bill_with_tip = float(bill) + float(bill)/100*float(tip)
+
+result = bill_with_tip/float(people)
+print(f"Each Person should pay: {round(result, 2)}$")
